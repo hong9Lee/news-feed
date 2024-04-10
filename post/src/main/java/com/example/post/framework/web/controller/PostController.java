09bb5ab06468358733d48e1e546730a8647e1cee
hong9Lee <hongtemp9mac@gmail.com>
2024-04-10 23:55:02 +0900

    feat: post 서버 설정 및 기본 설정

---
+++ post/src/main/java/com/example/post/framework/web/controller/PostController.java
@@ -0,0 +1,19 @@
+package com.example.post.framework.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post")
+@Slf4j
+@RequiredArgsConstructor
+public class PostController {
+
+
+
+
+
+
+}
